b49aa11d70a70ec5aaa4ec5ff46427006fce2c2f
gs-kbadal <110820836+gs-kbadal@users.noreply.github.com>
2022-11-01 15:21:48 +0530

    feedback changes

--- assignment1/src/app/login-form/login-form.component.ts
+++ assignment1/src/app/login-form/login-form.component.ts
@@ -1,60 +1,74 @@
-import { ThrowStmt } from "@angular/compiler"; // TODO Remove unused imports in all the components
-import { Component, OnInit } from "@angular/core";
+import { ThrowStmt } from "@angular/compiler";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { Router } from "@angular/router";
-import { AuthService } from "../auth.service";
+import { NzModalRef, NzModalService } from "ng-zorro-antd";
+import { EmployeeFormComponent } from "../employee-form/employee-form.component";
+import { employeeModel } from "../employee-form/employee-form.model";
 import { ApiService } from "../shared/api.service";
-import { login } from "./login.model";
 
 @Component({
-  selector: "app-login-form",
-  templateUrl: "./login-form.component.html",
-  styleUrls: ["./login-form.component.scss"],
+  selector: "app-list-view",
+  templateUrl: "./list-view.component.html",
+  styleUrls: ["./list-view.component.scss"],
 })
-export class LoginFormComponent implements OnInit {
-  validateForm!: FormGroup;
-  loginObj: login = new login(); // TODO: Not required use local variable instead
+export class ListViewComponent implements OnInit, OnDestroy {
+  employeeObj: employeeModel = new employeeModel();
 
-  userState: any; // Todo : Not required this variable
+  employeDetails!: employeeModel[];
+  validateForm: FormGroup;
+
+  employee_details: any;
+  employee_delete: any;
+  employee_update: any;
+
+  options = [
+    { label: "Male", value: "Male" },
+    { label: "Female", value: "Female" },
+  ];
+
+  isVisible = false;
+  isCancel = null;
+  isOk = null;
+
+  pageIndex = 1;
+  pageSize = 3;
 
   constructor(
-    private fb: FormBuilder,
-    private api: ApiService, // Todo: Use proper naming convention api instead od apiService
-    private router: Router,
-    private authService: AuthService
+    private api: ApiService,
+    private modalService: NzModalService,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit() {
+    this.getAllEmployeeDetails();
+
     this.validateForm = this.fb.group({
-      userName: [null, [Validators.required]],
-      password: [null, [Validators.required]],
-      remember: [true],
+      id: [null],
+      name: [null, [Validators.required]],
+      email: [null, [Validators.email, Validators.required]],
+      companyId: [null, [Validators.required]],
+      gender: [null, [Validators.required]],
+      doj: [null, [Validators.required]],
+      department: [null, [Validators.required]],
     });
   }
 
-  // for user logging
-  submitForm(): void {
+  // to update the employee details
+  updateForm(): void {
     if (this.validateForm.valid) {
-      this.loginObj.username = this.validateForm.value.userName; // Todo: Take directly .value instead of each value
-      this.loginObj.password = this.validateForm.value.password;
-      this.api.login(this.loginObj).subscribe((res) => { // Todo: unsubscribe mandatory in all the components
-        const user = res.find((a: any) => {
-          return (
-            a.username === this.loginObj.username &&
-            a.password === this.loginObj.password
-          );
-        });
-        if (user) {
-          alert("login successfull!!"); // Todo: Use notification component instead of alert in all places
-          this.userState = user;
-          localStorage.setItem('user', JSON.stringify(this.userState)); // Todo: Localstorage should be in service, check all the components
-          this.authService.login();
-          this.validateForm.reset();
-          this.router.navigate(["home","listview"]);
-        } else {
-          alert("invalid credentials!");
+      const id = this.validateForm.value.id;
+      this.employeeObj = this.validateForm.value;
+
+      this.employee_update = this.api.updateEmployee(this.employeeObj, id).subscribe(
+        (res: any) => {
+          alert("Employee details updated successfully!");
+          this.isVisible = false;
+          this.getAllEmployeeDetails();
+        },
+        (error: any) => {
+          console.log(error);
         }
-      });
+      );
     } else {
       Object.values(this.validateForm.controls).forEach((control) => {
         if (control.invalid) {
@@ -63,5 +77,58 @@ export class LoginFormComponent implements OnInit {
         }
       });
     }
+    this.validateForm.reset();
+  }
+
+  // to format the date according to MM/DD/YYYY
+  format(inputDate: any) {
+    let date: any, month: any, year: any;
+    const day = inputDate.slice(0, 10);
+
+    year = day.slice(0, 4);
+    month = day.slice(5, 7);
+    date = day.slice(8, 10);
+
+    return `${month}/${date}/${year}`;
+  }
+
+  // to get all the employee details
+  getAllEmployeeDetails() {
+    this.employee_details = this.api.getEmployee().subscribe((res) => {
+      this.employeDetails = res;
+      for (let index in this.employeDetails) {
+        this.employeDetails[index].doj = this.format(this.employeDetails[index].doj);
+      }
+    });
+  }
+
+  // to delete the employee details
+  deleteEmployee(row: any) {
+    this.employee_delete = this.api.deleteEmployee(row.id).subscribe((res) => {
+      alert("Employee deleted!");
+      this.getAllEmployeeDetails();
+    });
+  }
+
+  // to show edit form in modal and also prepopulate the form
+  showModal(data: any): void {
+    this.isVisible = true;
+    nzCancelText: null;
+    nzOkText: null;
+    this.api.getSingleEmployee(data.id).subscribe((res) => {
+      this.validateForm.setValue(res);
+    });
+  }
+
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+
+  // to unsubscribe
+  ngOnDestroy(): void {
+    // this.employee_update.unsubscribe();
+    // this.employee_details.unsubscribe();
+    // this.employee_delete.unsubscribe();
   }
 }
